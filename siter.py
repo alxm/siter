6b3ab919bcd16dcc518fec2b6f378a244e9b9a6f
Alex <artraid@gmail.com>
2011-04-22 15:08:18 -0400

    Added support for simple macros. Variables can be called from the page content, not just the template.

--- siter.py
+++ siter.py
@@ -19,6 +19,34 @@
 
 import os, re, string, sys
 
+def siter_error(error):
+    print "Siter Error: " + error
+    sys.exit(1)
+
+def siter_replace(text, bindings, functions):
+    for b in bindings:
+        text = string.replace(text, "${" + b + "}", bindings[b])
+
+    for f in functions:
+        (params, body) = functions[f]
+        calls = re.finditer("\$\{" + f + "\((.+)\)\}", text)
+
+        for call in calls:
+            whole = call.group(0).strip()
+            args = call.group(1).split("///")
+
+            if len(args) != len(params):
+                siter_error("Wrong number of arguments")
+
+            body2 = body
+
+            for i in range(0, len(args)):
+                body2 = string.replace(body2, "${" + params[i] + "}", args[i])
+
+            text = string.replace(text, whole, body2)
+
+    return text
+
 def siter(siter_dir):
     siter_pages = siter_dir + "/siter-pages"
     siter_template = siter_dir + "/siter-template.html"
@@ -32,42 +60,56 @@ def siter(siter_dir):
     template = None
 
     with open(siter_template, "r") as f:
-        template = string.Template(f.read())
+        template = f.read()
 
-    varbinder = re.compile("(.+?)=(.*)")
+    siter_re_define = re.compile("(.+?)=(.*)")      # id = value
+    siter_re_function = re.compile("(.+?)\((.+)\)") # func(args)
 
     for page_file in os.listdir(siter_pages):
         read_file = siter_pages + "/" + page_file
         write_file = siter_dir + "/" + page_file
 
-        with open(read_file, "r") as f:
-            print "Reading " + read_file
-
+        with open(read_file, "r") as r:
             doing_bindings = True
+
             bindings = {}
+            functions = {}
             page = ""
 
-            for line in f:
+            for line in r:
                 if doing_bindings:
-                    match = varbinder.search(line)
+                    # scan for id = value
+                    match = siter_re_define.search(line)
 
                     if match:
-                        bindings[match.group(1).strip()] = match.group(2).strip()
+                        name = match.group(1).strip()
+                        value = match.group(2).strip()
+
+                        # scan for func(args), originally func(args) = value
+                        match = siter_re_function.search(name)
+
+                        if match:
+                            name = match.group(1).strip()
+                            params = match.group(2).strip()
+
+                            functions[name] = (params.split(", "), value)
+                        else:
+                            bindings[name] = value
                     else:
                         doing_bindings = False
                         page = page + line
                 else:
                     page = page + line
 
-            bindings["page"] = page
+            # replace variables inside page content
+            bindings["page"] = siter_replace(page, bindings, functions)
 
-            with open(write_file, "w") as p:
+            # replace variables inside template
+            page = siter_replace(template, bindings, functions)
+
+            with open(write_file, "w") as w:
                 print "Writing " + write_file
-                p.write(template.safe_substitute(bindings))
-
-def siter_error(error):
-    print "Siter Error: " + error
-    sys.exit(1)
+                w.write(page)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
